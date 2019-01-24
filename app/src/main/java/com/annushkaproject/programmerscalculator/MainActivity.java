d6cd81f48424347e9681554508555bce368d20db
Mariia Saveleva <mariiasaveleva@gmail.com>
2019-01-24 15:37:09 +0200

    code cleanup

--- app/src/main/java/com/annushkaproject/programmerscalculator/MainActivity.java
+++ app/src/main/java/com/annushkaproject/programmerscalculator/MainActivity.java
@@ -47,10 +47,8 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_standard_mode) {
