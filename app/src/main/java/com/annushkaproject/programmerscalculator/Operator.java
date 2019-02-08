d21297cd50ab9ccaae9cbe8f852a2a854caed94f
Rukhsora Nazarova <rukhsorka@gmail.com>
2019-02-08 12:24:40 +0200

    added corresponding integer keys for each operator

--- app/src/main/java/com/annushkaproject/programmerscalculator/Operator.java
+++ app/src/main/java/com/annushkaproject/programmerscalculator/Operator.java
@@ -37,8 +37,40 @@ public enum Operator {
             default:
                 return false;
         }
-
     }
 
+    public int getNumberByOperator(Operator operator) {
+        switch (operator) {
+            case ADD:
+                return 0;
+            case SUBSTRUCT:
+                return 1;
+            case MULTIPLY:
+                return 2;
+            case DIVIDE:
+                return 3;
+            case PERCENT:
+                return 4;
+            default:
+                return 5;
+        }
+    }
+
+    public Operator getOperatorByNumber(int number) {
+        switch (number) {
+            case 0:
+                return ADD;
+            case 1:
+                return SUBSTRUCT;
+            case 2:
+                return MULTIPLY;
+            case 3:
+                return DIVIDE;
+            case 4:
+                return PERCENT;
+            default:
+                return UNKNOWN;
+        }
+    }
 }
 
