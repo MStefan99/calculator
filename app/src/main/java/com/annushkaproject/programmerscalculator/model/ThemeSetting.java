0331cf771f2f4753d2ba64551f92e34a910b0db9
Maria Saveleva <mariiasaveleva@gmail.com>
2019-02-12 15:00:20 +0000

    shared preferences util
    Approved-by: Mikhail Stefantsev <stephan05.08.67@gmail.com>
    Approved-by: Sora Nazarova (Rbooks999) <rukhsorka@gmail.com>

---
+++ app/src/main/java/com/annushkaproject/programmerscalculator/model/ThemeSetting.java
@@ -0,0 +1,38 @@
+package com.annushkaproject.programmerscalculator.model;
+
+import android.content.res.Resources;
+
+public enum ThemeSetting {
+    LIGHT,
+    DARK,
+    AUTO,
+    UNKNOWN;
+
+    public static int getNumberByThemeSetting(ThemeSetting themeSetting) {
+        switch (themeSetting) {
+            case LIGHT:
+                return 0;
+            case DARK:
+                return 1;
+            case AUTO:
+                return 2;
+
+            default:
+                return -1;
+        }
+    }
+
+    public static ThemeSetting getThemeSettingByNumber(int number) {
+        switch (number) {
+            case 0:
+                return ThemeSetting.LIGHT;
+            case 1:
+                return ThemeSetting.DARK;
+            case 2:
+                return ThemeSetting.AUTO;
+
+            default:
+                return ThemeSetting.UNKNOWN;
+        }
+    }
+}
