a46b16180c6577d2d6219bd55d85c69e15636156
Mariia Saveleva <mariiasaveleva@gmail.com>
2019-03-01 13:27:46 +0200

    updated standard division tests

--- app/src/test/java/com/annushkaproject/programmerscalculator/StandardTests/StandardDivisionTest.java
+++ app/src/test/java/com/annushkaproject/programmerscalculator/StandardTests/StandardDivisionTest.java
@@ -1,21 +1,48 @@
 package com.annushkaproject.programmerscalculator.StandardTests;
 
+import android.database.sqlite.SQLiteBlobTooBigException;
+
 import com.annushkaproject.programmerscalculator.model.CalculationModel;
 import com.annushkaproject.programmerscalculator.model.Value;
 import com.annushkaproject.programmerscalculator.utils.StandardOperationsUtil;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static com.annushkaproject.programmerscalculator.model.Operator.DIVIDE;
 import static org.junit.Assert.assertEquals;
 
 public class StandardDivisionTest {
     @Test
-    public void standardDivision_isCorrect() {
-        assertEquals(5, StandardOperationsUtil.calculateResultForTwoSidedOperator(new CalculationModel(new Value(25), new Value(5), DIVIDE)), 0); // Integer test
-        assertEquals(-5, StandardOperationsUtil.calculateResultForTwoSidedOperator(new CalculationModel(new Value(-25), new Value(5), DIVIDE)), 0); // Integer test
-        assertEquals(-0.00005, StandardOperationsUtil.calculateResultForTwoSidedOperator(new CalculationModel(new Value(-0.0001), new Value(2), DIVIDE)), 0); // Decimal test
-        assertEquals(1.5, StandardOperationsUtil.calculateResultForTwoSidedOperator(new CalculationModel(new Value(3), new Value(2), DIVIDE)), 0); // Decimal test
-        assertEquals(0.5, StandardOperationsUtil.calculateResultForTwoSidedOperator(new CalculationModel(new Value(1), new Value(2), DIVIDE)), 0); // Decimal test
+    public void testIwoIntegers() {
+        BigDecimal firstValue = BigDecimal.valueOf(25);
+        BigDecimal secondValue = BigDecimal.valueOf(5);
+        CalculationModel model = new CalculationModel(firstValue, secondValue, DIVIDE);
+        assertEquals(5, StandardOperationsUtil.calculateResultForTwoSidedOperator(model), 0);
+    }
+
+    @Test
+    public void testFirstFloat() {
+        BigDecimal firstValue = BigDecimal.valueOf(-0.0001);
+        BigDecimal secondValue = BigDecimal.valueOf(2);
+        CalculationModel model = new CalculationModel(firstValue, secondValue, DIVIDE);
+        assertEquals(-0.00005, StandardOperationsUtil.calculateResultForTwoSidedOperator(model), 0);
+    }
+
+    @Test
+    public void testFloatResult() {
+        BigDecimal firstValue = BigDecimal.valueOf(3);
+        BigDecimal secondValue = BigDecimal.valueOf(2);
+        CalculationModel model = new CalculationModel(firstValue, secondValue, DIVIDE);
+        assertEquals(1.5, StandardOperationsUtil.calculateResultForTwoSidedOperator(model), 0);
+    }
+
+    @Test
+    public void testFloatResultLessThanOne() {
+        BigDecimal firstValue = BigDecimal.valueOf(1);
+        BigDecimal secondValue = BigDecimal.valueOf(2);
+        CalculationModel model = new CalculationModel(firstValue, secondValue, DIVIDE);
+        assertEquals(0.5, StandardOperationsUtil.calculateResultForTwoSidedOperator(model), 0);
     }
 }
