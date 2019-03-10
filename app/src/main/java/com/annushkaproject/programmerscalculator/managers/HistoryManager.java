4451afc46edc3a96848f1466138d443080c60692
Maria Saveleva <mariiasaveleva@gmail.com>
2019-03-10 14:26:42 +0000

    realm classes javadoc
    Approved-by: Mikhail Stefantsev <stephan05.08.67@gmail.com>

--- app/src/main/java/com/annushkaproject/programmerscalculator/managers/HistoryManager.java
+++ app/src/main/java/com/annushkaproject/programmerscalculator/managers/HistoryManager.java
@@ -9,6 +9,9 @@ import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 import java.util.Date;
 
+/**
+ * Singleton for handling communication with Realm database. Requires Realm.init() call once before usage.
+ */
 public class HistoryManager {
 
     private static final String DATABASE_NAME = "calculator.realm";
@@ -24,10 +27,18 @@ public class HistoryManager {
         return managerInstance;
     }
 
+    /**
+     * Debug method for printing current calculator.realm database file's path.
+     */
     public void printDatabaseLocation() {
         System.out.println("Realm db path: " + realm.getPath());
     }
 
+    /**
+     * Takes result value and saves it to database.
+     *
+     * @param resultString String with result value.
+     */
     public void save(String resultString) {
         realm.beginTransaction();
         HistoryResult result = realm.createObject(HistoryResult.class);
@@ -35,6 +46,11 @@ public class HistoryManager {
         realm.commitTransaction();
     }
 
+    /**
+     * Gets result object, remove from database if exists.
+     *
+     * @param resultToDelete HistoryResult object to delete.
+     */
     public void deleteResult(HistoryResult resultToDelete) {
         RealmResults<HistoryResult> results = realm.where(HistoryResult.class).equalTo("id", resultToDelete.getId()).findAll();
         if (results.size() > 0) {
@@ -44,6 +60,10 @@ public class HistoryManager {
         }
     }
 
+    /**
+     * Fetch all saved history data.
+     * @return ArrayList of all founded HistoryResults that were saved previously.
+     */
     public ArrayList<HistoryResult> fetchAllHistoryResults() {
         ArrayList<HistoryResult> results = new ArrayList(realm.where(HistoryResult.class).findAll());
         return results;
