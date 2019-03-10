31b189bf05f7b440d6e7805284c6685d1ad1f3c8
Mariia Saveleva <mariiasaveleva@gmail.com>
2019-03-10 15:22:41 +0200

    implemented first version of History Manager and History Result model object

--- app/src/main/java/com/annushkaproject/programmerscalculator/managers/HistoryManager.java
+++ app/src/main/java/com/annushkaproject/programmerscalculator/managers/HistoryManager.java
@@ -1,7 +1,13 @@
 package com.annushkaproject.programmerscalculator.managers;
 
+import com.annushkaproject.programmerscalculator.model.HistoryResult;
+
+import java.util.ArrayList;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
+import java.util.Date;
 
 public class HistoryManager {
 
@@ -12,16 +18,35 @@ public class HistoryManager {
     RealmConfiguration config = new RealmConfiguration.Builder().name(DATABASE_NAME).build();
     private Realm realm;
 
-    public static HistoryManager getSharedInstance() {
-        return managerInstance;
-    }
-
     private HistoryManager() {
         Realm.setDefaultConfiguration(config);
         realm = Realm.getDefaultInstance();
     }
 
+    public static HistoryManager getSharedInstance() {
+        return managerInstance;
+    }
+
     public void printDatabaseLocation() {
         System.out.println("Realm db path: " + realm.getPath());
+    }
+
+    public void save(String resultString) {
+        realm.beginTransaction();
+        HistoryResult result = realm.createObject(HistoryResult.class);
+        result.setResult(resultString);
+        realm.commitTransaction();
+    }
+
+    public void deleteObjectWithDate(String result, Date date) {
+        RealmResults<HistoryResult> results = realm.where(HistoryResult.class).equalTo(result, date).findAll();
+        realm.beginTransaction();
+        results.deleteAllFromRealm();
+        realm.commitTransaction();
+    }
+
+    public ArrayList<HistoryResult> fetchAllHistoryResults() {
+        ArrayList<HistoryResult> results = new ArrayList(realm.where(HistoryResult.class).findAll());
+        return results;
     }
 }
